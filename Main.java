069c352b7e395d4090f64c4c035876396310859f
Ondřej Krejčíř <ondrej.krejcir@antee.cz>
2014-05-04 23:39:13 +0200

    Data pro nejhorší případ

--- Main.java
+++ Main.java
@@ -5,70 +5,50 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Subject> subjects = new ArrayList<Subject>();
 
-        // Generate ASF schedule
-        Subject ASF = new Subject("ASF", new Lecture(Day.TUESDAY, 11, 2));
-        ASF.addExercise(new Exercise(Day.TUESDAY, 7, 2));
-        ASF.addExercise(new Exercise(Day.TUESDAY, 9, 2));
-        ASF.addExercise(new Exercise(Day.TUESDAY, 5, 2));
-        ASF.addExercise(new Exercise(Day.WEDNESDAY, 9, 2));
-        ASF.addExercise(new Exercise(Day.THURSDAY, 7, 2));
-        ASF.addExercise(new Exercise(Day.THURSDAY, 3, 2));
-        ASF.addExercise(new Exercise(Day.THURSDAY, 5, 2));
-        ASF.addExercise(new Exercise(Day.TUESDAY, 13, 2));
-        subjects.add(ASF);
+        Subject A = new Subject("A", new Lecture(Day.MONDAY, 1, 1));
+        A.addExercise(new Exercise(Day.FRIDAY, 2, 1));
+        A.addExercise(new Exercise(Day.FRIDAY, 2, 1));
+        A.addExercise(new Exercise(Day.FRIDAY, 2, 1));
+        A.addExercise(new Exercise(Day.FRIDAY, 2, 1));
+        A.addExercise(new Exercise(Day.MONDAY, 2, 1));
+        subjects.add(A);
 
-        // Generate KAJ schedule
-        Subject KAJ = new Subject("KAJ", new Lecture(Day.MONDAY, 3, 2));
-        KAJ.addExercise(new Exercise(Day.MONDAY, 5, 2));
-        KAJ.addExercise(new Exercise(Day.MONDAY, 7, 2));
-        KAJ.addExercise(new Exercise(Day.MONDAY, 11, 2));
-        subjects.add(KAJ);
+        Subject B = new Subject("B", new Lecture(Day.TUESDAY, 1, 1));
+        B.addExercise(new Exercise(Day.TUESDAY, 2, 1));
+        B.addExercise(new Exercise(Day.TUESDAY, 2, 1));
+        B.addExercise(new Exercise(Day.TUESDAY, 2, 1));
+        B.addExercise(new Exercise(Day.TUESDAY, 2, 1));
+        B.addExercise(new Exercise(Day.TUESDAY, 2, 1));
+        subjects.add(B);
 
-        // Generate TAL schedule
-        Subject TAL = new Subject("TAL", new Lecture(Day.TUESDAY, 2, 3));
-        TAL.addExercise(new Exercise(Day.THURSDAY, 1, 2));
-        TAL.addExercise(new Exercise(Day.THURSDAY, 3, 2));
-        TAL.addExercise(new Exercise(Day.THURSDAY, 5, 2));
-        subjects.add(TAL);
+        Subject E = new Subject("C", new Lecture(Day.WEDNESDAY, 1, 1));
+        E.addExercise(new Exercise(Day.WEDNESDAY, 2, 1));
+        E.addExercise(new Exercise(Day.WEDNESDAY, 2, 1));
+        E.addExercise(new Exercise(Day.WEDNESDAY, 2, 1));
+        E.addExercise(new Exercise(Day.WEDNESDAY, 2, 1));
+        E.addExercise(new Exercise(Day.WEDNESDAY, 2, 1));
+        subjects.add(E);
 
-        // Generate KO schedule
-        Subject KO = new Subject("KO", new Lecture(Day.TUESDAY, 5, 3));
-        KO.addExercise(new Exercise(Day.THURSDAY, 1, 2));
-        KO.addExercise(new Exercise(Day.THURSDAY, 3, 2));
-        KO.addExercise(new Exercise(Day.THURSDAY, 5, 2));
-        KO.addExercise(new Exercise(Day.THURSDAY, 7, 2));
-        KO.addExercise(new Exercise(Day.WEDNESDAY, 9, 2));
-        KO.addExercise(new Exercise(Day.WEDNESDAY, 11, 2));
-        KO.addExercise(new Exercise(Day.WEDNESDAY, 13, 2));
-        KO.addExercise(new Exercise(Day.THURSDAY, 11, 2));
-        KO.addExercise(new Exercise(Day.THURSDAY, 9, 2));
-        subjects.add(KO);
+        Subject D = new Subject("D", new Lecture(Day.THURSDAY, 1, 1));
+        D.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        D.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        D.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        D.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        D.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        subjects.add(D);
 
-        // Generate OSP schedule
-        Subject OSP = new Subject("OSP", new Lecture(Day.WEDNESDAY, 9, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 11, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 3, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 5, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 7, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 9, 2));
-        OSP.addExercise(new Exercise(Day.THURSDAY, 13, 2));
-        subjects.add(OSP);
-
-        // Generate PDA schedule
-        Subject PDA = new Subject("PDA", new Lecture(Day.MONDAY, 11, 2));
-        PDA.addExercise(new Exercise(Day.MONDAY, 5, 2));
-        PDA.addExercise(new Exercise(Day.MONDAY, 7, 2));
-        PDA.addExercise(new Exercise(Day.MONDAY, 9, 2));
-        subjects.add(PDA);
+        Subject C = new Subject("E", new Lecture(Day.FRIDAY, 1, 1));
+        C.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        C.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        C.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        C.addExercise(new Exercise(Day.THURSDAY, 2, 1));
+        C.addExercise(new Exercise(Day.FRIDAY, 2, 1));
+        subjects.add(C);
 
         Solver solver = new Solver(subjects);
         Printer printer = new Printer();
 
         try {
-            solver.allowLectureCoversLimitation();
-            solver.allowExerciseCoversLecture();
-            solver.setLatestItem(12);
-            solver.setEarliestItem(5);
             Schedule schedule = solver.solve();
             printer.print(schedule, "./schedule.html");
         } catch (SmartScheduleException e) {
